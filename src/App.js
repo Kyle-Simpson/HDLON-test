10f5f111ef147d349c882a8cd30aec9ef7d840ae
Kyle Simpson <kyle3381@gmail.com>
2019-06-14 13:58:42 -0700

    Screen size functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, CardImg, CardTitle, CardText, CardBody, Container, Row, Col } from 'reactstrap';
+import { Card, CardImg, CardTitle, CardBody, Container, Row, Col } from 'reactstrap';
 import './App.css';
 
 function App() {
@@ -48,7 +48,144 @@ function App() {
             </Container>
           </CardBody>
         </Card>
+
+        <h3>Start of the Workday - <i>Inicio del día de trabajo</i></h3>
+        <Card>
+          <CardBody>
+            <Container>
+              <Row>
+                <b>Good Morning</b>
+              </Row>
+              <Row>
+                [gud mornin]
+            </Row>
+              <Row>
+                <i>Buenos días</i>
+              </Row>
+              <audio controls>
+                <source src='./audio/mp3/1.a.mp3' type='audio/mp3'></source>
+              </audio>
+            </Container>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <Container>
+              <Row>
+                <b>I’m ___________. I am a worker from ____________. Nice to meet  you.</b>
+              </Row>
+              <Row>
+                [aim ___________. Aim a wurker fram __________. Nais tu mit yu]
+            </Row>
+              <Row>
+                <i>Yo soy __________. Soy un trabajador de __________. Es un placer conocerlo/a.</i>
+              </Row>
+              <audio controls>
+                <source src='./audio/mp3/1.b.mp3' type='audio/mp3'></source>
+              </audio>
+            </Container>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <Container>
+              <Row>
+                <b>What will I be doing today?</b>
+              </Row>
+              <Row>
+                [Guat wil ai bi duin tudei]
+            </Row>
+              <Row>
+                <i>¿Qué voy a hacer hoy?</i>
+              </Row>
+              <audio controls>
+                <source src='./audio/mp3/1.c.mp3' type='audio/mp3'></source>
+              </audio>
+            </Container>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <Container>
+              <Row>
+                <b>What type of gardening work do you need done? </b>
+              </Row>
+              <Row>
+                [Guat taip of gardenin work du yu nid dan]
+            </Row>
+              <Row>
+                <i>¿Qué trabajo necesita hacer en el jardin?</i>
+              </Row>
+              <audio controls>
+                <source src='./audio/mp3/1.d.mp3' type='audio/mp3'></source>
+              </audio>
+            </Container>
+          </CardBody>
+        </Card>
+
+        <h3>Gardening Tools & Materials - <i>Herramientas y materiales para jardinería</i></h3>
+        <Card>
+          <CardImg style={{ width: '80%', height: '50vh', 'marginLeft': 'auto', 'marginRight': 'auto' }} src='./images/garden_rake.pdf' alt='garden rake' />
+          <CardBody>
+            <Container>
+              <Row>
+                <b>Garden rake </b>
+              </Row>
+              <Row>
+                [garden reik]
+            </Row>
+              <Row>
+                <i>rastrillo de jardin</i>
+              </Row>
+              <audio controls>
+                <source src='./audio/mp3/2.j.mp3' type='audio/mp3'></source>
+              </audio>
+            </Container>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg style={{ width: '80%', height: '50vh', 'marginLeft': 'auto', 'marginRight': 'auto' }} src='./images/broom.pdf' alt='garden rake' />
+          <CardBody>
+            <Container>
+              <Row>
+                <b>Broom</b>
+              </Row>
+              <Row>
+                [brum]
+            </Row>
+              <Row>
+                <i>escoba</i>
+              </Row>
+              <audio controls>
+                <source src='./audio/mp3/2.q.mp3' type='audio/mp3'></source>
+              </audio>
+            </Container>
+          </CardBody>
+        </Card>
+
+        <h3>Elements of the Garden - <i>Elementos en un jardín</i></h3>
+
+        <h3>Questions for the Employer - <i>Preguntas para el/la empleador/a</i></h3>
+
+        <h3>Instructions for the Gardener - <i>Instrucciones para el/la jardinero/a</i></h3>
+
+        <h3>Common Gardening Tasks - <i>Tareas comunes de jardinería</i></h3>
+
+        <h3>Health and Safety - <i>Equipo de seguridad</i></h3>
+
+        <h3>Negotiating Pay - <i>Negociación de pago</i></h3>
+
+        <h3>Questions for Clarification - <i>Preguntas para aclarar</i></h3>
+
       </div>
+
+
+
+
+
+
+
+
 
       <div className="bigScreen">
         <Container>
@@ -106,117 +243,160 @@ function App() {
             </Col>
           </Row>
         </Container>
+
+        <a id="start-of-workday"><h3>Start of the Workday - <i>Inicio del día de trabajo</i></h3></a>
+        <Row>
+          <Col>
+            <Card>
+              <CardBody>
+                <Container>
+                  <Row>
+                    <b>Good Morning</b>
+                  </Row>
+                  <Row>
+                    [gud mornin]
+            </Row>
+                  <Row>
+                    <i>Buenos días</i>
+                  </Row>
+                  <audio controls>
+                    <source src='./audio/mp3/1.a.mp3' type='audio/mp3'></source>
+                  </audio>
+                </Container>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+                <Container>
+                  <Row>
+                    <b>I’m ___________. I am a worker from ____________. Nice to meet  you.</b>
+                  </Row>
+                  <Row>
+                    [aim ___________. Aim a wurker fram __________. Nais tu mit yu]
+            </Row>
+                  <Row>
+                    <i>Yo soy __________. Soy un trabajador de __________. Es un placer conocerlo/a.</i>
+                  </Row>
+                  <audio controls>
+                    <source src='./audio/mp3/1.b.mp3' type='audio/mp3'></source>
+                  </audio>
+                </Container>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card>
+              <CardBody>
+                <Container>
+                  <Row>
+                    <b>What will I be doing today?</b>
+                  </Row>
+                  <Row>
+                    [Guat wil ai bi duin tudei]
+            </Row>
+                  <Row>
+                    <i>¿Qué voy a hacer hoy?</i>
+                  </Row>
+                  <audio controls>
+                    <source src='./audio/mp3/1.c.mp3' type='audio/mp3'></source>
+                  </audio>
+                </Container>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+                <Container>
+                  <Row>
+                    <b>What type of gardening work do you need done? </b>
+                  </Row>
+                  <Row>
+                    [Guat taip of gardenin work du yu nid dan]
+            </Row>
+                  <Row>
+                    <i>¿Qué trabajo necesita hacer en el jardin?</i>
+                  </Row>
+                  <audio controls>
+                    <source src='./audio/mp3/1.d.mp3' type='audio/mp3'></source>
+                  </audio>
+                </Container>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+
+
+
+
+        <a id="tools-and-materials"><h3>Gardening Tools & Materials - <i>Herramientas y materiales para jardinería</i></h3></a>
+        <Row>
+          <Col>
+            <Card>
+              <CardImg style={{ width: '80%', height: '50vh', 'marginLeft': 'auto', 'marginRight': 'auto' }} src='./images/garden_rake.pdf' alt='garden rake' />
+              <CardBody>
+                <Container>
+                  <Row>
+                    <b>Garden rake </b>
+                  </Row>
+                  <Row>
+                    [garden reik]
+            </Row>
+                  <Row>
+                    <i>rastrillo de jardin</i>
+                  </Row>
+                  <audio controls>
+                    <source src='./audio/mp3/2.j.mp3' type='audio/mp3'></source>
+                  </audio>
+                </Container>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardImg style={{ width: '80%', height: '50vh', 'marginLeft': 'auto', 'marginRight': 'auto' }} src='./images/broom.pdf' alt='garden rake' />
+              <CardBody>
+                <Container>
+                  <Row>
+                    <b>Broom</b>
+                  </Row>
+                  <Row>
+                    [brum]
+            </Row>
+                  <Row>
+                    <i>escoba</i>
+                  </Row>
+                  <audio controls>
+                    <source src='./audio/mp3/2.q.mp3' type='audio/mp3'></source>
+                  </audio>
+                </Container>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+
+
+
+        <a id="elements-of-garden"><h3>Elements of the Garden - <i>Elementos en un jardín</i></h3></a>
+
+        <a id="employer-questions"><h3>Questions for the Employer - <i>Preguntas para el/la empleador/a</i></h3></a>
+
+        <a id="gardener-instructions"><h3>Instructions for the Gardener - <i>Instrucciones para el/la jardinero/a</i></h3></a>
+
+        <a id="common-tasks"><h3>Common Gardening Tasks - <i>Tareas comunes de jardinería</i></h3></a>
+
+        <a id="health-and-safety"><h3>Health and Safety - <i>Equipo de seguridad</i></h3></a>
+
+        <a id="negotiating-pay"><h3>Negotiating Pay - <i>Negociación de pago</i></h3></a>
+
+        <a id="clarification-questions"><h3>Questions for Clarification - <i>Preguntas para aclarar</i></h3></a>
+
       </div>
 
-
-      <a id="start-of-workday"><h3>Start of the Workday - <i>Inicio del día de trabajo</i></h3></a>
-      <Card>
-        <CardBody>
-          <Container>
-            <Row>
-              <b>Good Morning</b>
-            </Row>
-            <Row>
-              [gud mornin]
-            </Row>
-            <Row>
-              <i>Buenos días</i>
-            </Row>
-            <audio controls>
-              <source src='./audio/mp3/1.a.mp3' type='audio/mp3'></source>
-            </audio>
-          </Container>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardBody>
-          <Container>
-            <Row>
-              <b>I’m ___________. I am a worker from ____________. Nice to meet  you.</b>
-            </Row>
-            <Row>
-              [aim ___________. Aim a wurker fram __________. Nais tu mit yu]
-            </Row>
-            <Row>
-              <i>Yo soy __________. Soy un trabajador de __________. Es un placer conocerlo/a.</i>
-            </Row>
-            <audio controls>
-              <source src='./audio/mp3/1.b.mp3' type='audio/mp3'></source>
-            </audio>
-          </Container>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardBody>
-          <Container>
-            <Row>
-              <b>What will I be doing today?</b>
-            </Row>
-            <Row>
-              [Guat wil ai bi duin tudei]
-            </Row>
-            <Row>
-              <i>¿Qué voy a hacer hoy?</i>
-            </Row>
-            <audio controls>
-              <source src='./audio/mp3/1.c.mp3' type='audio/mp3'></source>
-            </audio>
-          </Container>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardBody>
-          <Container>
-            <Row>
-              <b>What type of gardening work do you need done? </b>
-            </Row>
-            <Row>
-              [Guat taip of gardenin work du yu nid dan]
-            </Row>
-            <Row>
-              <i>¿Qué trabajo necesita hacer en el jardin?</i>
-            </Row>
-            <audio controls>
-              <source src='./audio/mp3/1.d.mp3' type='audio/mp3'></source>
-            </audio>
-          </Container>
-        </CardBody>
-      </Card>
-
-      <a id="tools-and-materials"><h3>Gardening Tools & Materials - <i>Herramientas y materiales para jardinería</i></h3></a>
-      <Card>
-        <CardImg style={{width:'80%', height:'50vh', 'marginLeft':'auto', 'marginRight':'auto'}} src='./images/garden_rake.pdf' alt='garden rake' />
-        <CardBody>
-          <Container>
-            <Row>
-              <b>Garden rake </b>
-            </Row>
-            <Row>
-              [garden reik]
-            </Row>
-            <Row>
-              <i>rastrillo de jardin</i>
-            </Row>
-            <audio controls>
-              <source src='./audio/mp3/2.j.mp3' type='audio/mp3'></source>
-            </audio>
-          </Container>
-        </CardBody>
-      </Card>
-
-      <a id="elements-of-garden"><h3>Elements of the Garden - <i>Elementos en un jardín</i></h3></a>
-
-      <a id="employer-questions"><h3>Questions for the Employer - <i>Preguntas para el/la empleador/a</i></h3></a>
-
-      <a id="gardener-instructions"><h3>Instructions for the Gardener - <i>Instrucciones para el/la jardinero/a</i></h3></a>
-
-      <a id="common-tasks"><h3>Common Gardening Tasks - <i>Tareas comunes de jardinería</i></h3></a>
-
-      <a id="health-and-safety"><h3>Health and Safety - <i>Equipo de seguridad</i></h3></a>
-
-      <a id="negotiating-pay"><h3>Negotiating Pay - <i>Negociación de pago</i></h3></a>
-
-      <a id="clarification-questions"><h3>Questions for Clarification - <i>Preguntas para aclarar</i></h3></a>
     </div>
   );
 }
