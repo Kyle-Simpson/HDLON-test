ff1ba014bd739b21476c621de434473e67f6a086
Kyle Simpson <kyle3381@gmail.com>
2019-06-13 17:55:32 -0700

    Updates

---
+++ precache-manifest.2d2753b6c1a2343a9644ead0090f2d91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a882a1db1208fb18c4484b87c8ee8158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c71fffb275e55e7dcd1",
+    "url": "/static/css/main.3dd3b0f6.chunk.css"
+  },
+  {
+    "revision": "1ea429c901e0b32ec947",
+    "url": "/static/js/2.b5d72d48.chunk.js"
+  },
+  {
+    "revision": "2c71fffb275e55e7dcd1",
+    "url": "/static/js/main.2a043ee6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb3aa3d3dfaaee7c3b9d342b0c1da9e0",
+    "url": "/static/media/Avenir.eb3aa3d3.ttc"
+  },
+  {
+    "revision": "7797713384091e6cac80daf5b147e10b",
+    "url": "/static/media/Butler_Black.77977133.otf"
+  }
+]);
